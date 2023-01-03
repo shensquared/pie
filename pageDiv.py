e7dc0057578242cba7bda7810c7b82d4819f5006
Shen Shen <shenshen@mit.edu>
2023-01-03 14:38:06 -0500

    styles

--- pageDiv.py
+++ pageDiv.py
@@ -14,7 +14,7 @@ tim_banner = html.Center(
         html.Div(
             [
                 dcc.Markdown(
-                    """### ❤️ make wide tim some pie 🥧 with the registrar enrollment sheets...""",
+                    """#### ❤️ make wide tim some pie 🥧 with the registrar enrollment sheets...""",
                     id="topBanner",
                 ),
                 # dcc.Markdown("""##### with the registrar enrollment sheets..."""),
@@ -41,14 +41,29 @@ upload_block = dcc.Upload(
     id="upload-data",
     children=html.Div(
         [
+            html.Br(),
             dcc.Markdown(
-                """Support both `classlst.xls` and `prereg.xls`. Uploaded data is **only** stored in your current browser session (i.e., the data evaporates once the tab is refreshed.)"""
+                [
+                    """ ###### Support `classlst.xls` and `prereg.xls`.""",
+                    """###### Drag/Drop sheets into the box, or click the box to select files""",
+                    """Uploaded is stored **only** in your current browser session; i.e., it evaporates once the tab is refreshed.""",
+                    # """# Click WideTim to refresh""",
+                ],
+                style={
+                    "width": "90%",
+                    "lineHeight": "30px",
+                    "borderWidth": "1.5px",
+                    "borderStyle": "dashed",
+                    "borderRadius": "15px",
+                    "textAlign": "center",
+                    "margin": "auto",
+                },
             ),
-            "Drag/Drop the sheets or ",
-            html.A("Select Files"),
+            # "Drag/Drop the sheets or ",
+            # html.A("Select Files"),
         ],
         style={
-            "width": "90%",
+            "width": "80%",
             "lineHeight": "30px",
             # "borderWidth": "1.5px",
             # "borderStyle": "dashed",
@@ -62,10 +77,6 @@ upload_block = dcc.Upload(
     multiple=True,
     style={
         "width": "77%",
-        "lineHeight": "30px",
-        "borderWidth": "1.5px",
-        "borderStyle": "dashed",
-        "borderRadius": "15px",
         "textAlign": "center",
         "margin": "auto",
         "display": "flex",
@@ -75,15 +86,19 @@ upload_block = dcc.Upload(
 
 demo_banner_block = html.Center(
     [
-        "Or, get a taste of the pie without uploading your own sheet... ",
-        html.Button("see half-baked example", id="demo", style={"marginLeft": "20px"}),
+        dcc.Markdown(
+            """###### Or, get a taste of the **interactive** pie without uploading your own sheet..."""
+        ),
+        html.Button(
+            "some half-baked examples", id="demo", style={"marginLeft": "20px"}
+        ),
     ],
     id="demo_banner",
 )
 
 dept_or_year_upload = html.Div(
     [
-        "Slice the last pie by",
+        "Slice the pie crust by",
         dcc.RadioItems(
             ["dept", "year"],
             value="dept",
@@ -97,7 +112,7 @@ dept_or_year_upload = html.Div(
 
 dept_or_year_example = html.Div(
     [
-        "Slice the last pie by",
+        "Slice the pie crust by",
         dcc.RadioItems(
             ["dept", "year"],
             value="dept",
@@ -119,8 +134,7 @@ pie_controls = html.Center(
                     list(div_paras.keys())[:-1], value="6.390", id="courseNumber"
                 ),
             ],
-            # style={"width": "15%", "float": "left"},
-            style={"margin": "auto", "width": "30%"},
+            # style={"margin": "auto", "width": "30%"},
         ),
         html.Div(
             style={"width": "5%", "float": "left"},
