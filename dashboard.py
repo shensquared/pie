a48def7c2015b7adaa496dcfa0127a52e96d338e
Shen Shen <shenshen@mit.edu>
2023-01-06 01:20:25 -0500

    theme

--- dashboard.py
+++ dashboard.py
@@ -7,8 +7,9 @@ from pageDiv import *
 
 import dash_bootstrap_components as dbc
 
+# https://dashcheatsheet.pythonanywhere.com/
 # external_stylesheets = ["assets/upstream.css"]
-external_stylesheets = [dbc.themes.BOOTSTRAP]
+external_stylesheets = [dbc.themes.MATERIA, dbc.icons.BOOTSTRAP]
 # external_stylesheets = []
 server = flask.Flask(__name__)
 app = Dash(
