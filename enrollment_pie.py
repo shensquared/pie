07568c80a965eb0a7f44e541b1355ff397172a9e
Shen Shen <shenshen@mit.edu>
2023-01-08 18:33:53 -0500

    add value

--- enrollment_pie.py
+++ enrollment_pie.py
@@ -125,7 +125,7 @@ def enrollment_chart(df, CourseTitle="", dept_or_year="dept"):
     )
     # fig.update_traces(hovertemplate="Count: %{value}<extra></extra>")
     # fig.update_traces(textinfo="label+percent entry+percent parent+value")
-    fig.update_traces(textinfo="label")
+    fig.update_traces(textinfo="label+value")
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
     # fig.update_layout(
     #     title={
