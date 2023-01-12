be38e3effab76afa47c4698ae5f1d55fff13f4e1
Shen Shen <shenshen@mit.edu>
2023-01-11 23:49:17 -0500

    middleware?

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from dashboard import app
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 server = app.server
-
+server.wsgi_app = ProxyFix(server.wsgi_app, x_for=1, x_host=1)
 if __name__ == "__main__":
-    app.run()
+    app.run(APPLICATION_ROOT="/pie")
