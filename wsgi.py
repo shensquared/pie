b533f68faa4062c052acea4530a4c6e8614bd754
Shen Shen <shenshen@mit.edu>
2023-01-12 01:50:00 -0500

    fix

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,13 @@
 from dashboard import app
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+# https://github.com/noirbizarre/flask-restplus/issues/132
 
-from reverse_proxy import FlaskReverseProxied
+# from reverse_proxy import FlaskReverseProxied
 
 
 server = app.server
-server.wsgi_app = ProxyFix(server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+server.wsgi_app = ProxyFix(server.wsgi_app, x_proto=1, x_host=1)
 # server = FlaskReverseProxied(server)
 if __name__ == "__main__":
     app.run()
