6dc06f7d9715f7a7e3bbd09aefcab11ce8356073
Shen Shen <shenshen@mit.edu>
2023-01-11 20:09:37 -0500

    path fix?

--- dashboard.py
+++ dashboard.py
@@ -17,8 +17,8 @@ app = Dash(
     external_stylesheets=external_stylesheets,
     server=server,
     suppress_callback_exceptions=True,
-    routes_pathname_prefix="/pie/",
-    requests_pathname_prefix="/pie/",
+    routes_pathname_prefix="/pie",
+    requests_pathname_prefix="/pie",
 )
 app.title = "Tim ❤ Enrollment Pie"
 
