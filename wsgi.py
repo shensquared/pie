474c661150cae2d34bd279e087f4d352f208abf7
Shen Shen <shenshen@mit.edu>
2023-01-12 00:15:08 -0500

    another fix?

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ from reverse_proxy import FlaskReverseProxied
 
 
 server = app.server
-# server.wsgi_app = ProxyFix(server.wsgi_app, x_for=1, x_host=1)
-server = FlaskReverseProxied(server)
+server.wsgi_app = ProxyFix(server.wsgi_app, x_for=1, x_host=1)
+# server = FlaskReverseProxied(server)
 if __name__ == "__main__":
     app.run(APPLICATION_ROOT="/pie")
