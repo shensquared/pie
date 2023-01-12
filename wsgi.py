a60d79d8ab01103aba9d7821fc7dbb6c9ff0ae9b
Shen Shen <shenshen@mit.edu>
2023-01-12 00:25:23 -0500

    no root

--- wsgi.py
+++ wsgi.py
@@ -9,4 +9,4 @@ server = app.server
 server.wsgi_app = ProxyFix(server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 # server = FlaskReverseProxied(server)
 if __name__ == "__main__":
-    app.run(APPLICATION_ROOT="/pie")
+    app.run()
