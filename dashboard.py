7bb233faa57a02b8243219d2afb98aaeefc202de
Shen Shen <shenshen@mit.edu>
2023-01-12 23:06:46 -0500

    reverse proxy when declaration

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,49 @@ import dash_bootstrap_components as dbc
 # external_stylesheets = ["assets/upstream.css"]
 external_stylesheets = [dbc.themes.MATERIA, dbc.icons.BOOTSTRAP]
 # external_stylesheets = []
+
+
+class ReverseProxied(object):
+    # https://web.archive.org/web/20131129080707/http://flask.pocoo.org/snippets/35/
+
+    """Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    """
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get("HTTP_X_SCRIPT_NAME", "")
+        if script_name:
+            environ["SCRIPT_NAME"] = script_name
+            path_info = environ["PATH_INFO"]
+            if path_info.startswith(script_name):
+                environ["PATH_INFO"] = path_info[len(script_name) :]
+
+        scheme = environ.get("HTTP_X_SCHEME", "")
+        if scheme:
+            environ["wsgi.url_scheme"] = scheme
+        return self.app(environ, start_response)
+
+
 server = flask.Flask(__name__)
+
+server.wsgi_app = ReverseProxied(server.wsgi_app)
+
 app = Dash(
     __name__,
     external_stylesheets=external_stylesheets,
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from dashboard import app
-from werkzeug.middleware.proxy_fix import ProxyFix
+
+# from werkzeug.middleware.proxy_fix import ProxyFix
 
 # https://github.com/noirbizarre/flask-restplus/issues/132
 
@@ -10,42 +11,6 @@ server = app.server
 # server.wsgi_app = ProxyFix(server.wsgi_app, x_proto=1, x_host=1)
 
 
-class ReverseProxied(object):
-    """Wrap the application in this middleware and configure the
-    front-end server to add these headers, to let you quietly bind
-    this to a URL other than / and to an HTTP scheme that is
-    different than what is used locally.
-
-    In nginx:
-    location /myprefix {
-        proxy_pass http://192.168.0.1:5001;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
-        }
-
-    :param app: the WSGI application
-    """
-
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        script_name = environ.get("HTTP_X_SCRIPT_NAME", "")
-        if script_name:
-            environ["SCRIPT_NAME"] = script_name
-            path_info = environ["PATH_INFO"]
-            if path_info.startswith(script_name):
-                environ["PATH_INFO"] = path_info[len(script_name) :]
-
-        scheme = environ.get("HTTP_X_SCHEME", "")
-        if scheme:
-            environ["wsgi.url_scheme"] = scheme
-        return self.app(environ, start_response)
-
-
-server.wsgi_app = ReverseProxied(server.wsgi_app)
 # server = FlaskReverseProxied(server)
 if __name__ == "__main__":
     app.run()
