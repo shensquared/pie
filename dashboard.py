a75e7bc07753bdcd08d9c1d75a3a901300a81b0a
Shen Shen <shenshen@mit.edu>
2023-01-13 21:18:35 -0500

    prod separate from local?

--- dashboard.py
+++ dashboard.py
@@ -59,8 +59,6 @@ app = Dash(
     external_stylesheets=external_stylesheets,
     server=server,
     suppress_callback_exceptions=True,
-    routes_pathname_prefix="/",
-    requests_pathname_prefix="/pie/",
 )
 app.title = "Tim ❤ Enrollment Pie"
 
