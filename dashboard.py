51593b247869efc60bc6a2ef9a479514a48e7178
Shen Shen <shenshen@mit.edu>
2023-01-13 21:19:05 -0500

    Revert "prod separate from local?"
    This reverts commit a75e7bc07753bdcd08d9c1d75a3a901300a81b0a.

--- dashboard.py
+++ dashboard.py
@@ -59,6 +59,8 @@ app = Dash(
     external_stylesheets=external_stylesheets,
     server=server,
     suppress_callback_exceptions=True,
+    routes_pathname_prefix="/",
+    requests_pathname_prefix="/pie/",
 )
 app.title = "Tim ❤ Enrollment Pie"
 
--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ from dashboard import app
 # https://github.com/noirbizarre/flask-restplus/issues/132
 
 # from reverse_proxy import FlaskReverseProxied
-app.config.update({"routes_pathname_prefix": "/", "requests_pathname_prefix": "/pie/"})
+
 
 server = app.server
 # server.wsgi_app = ProxyFix(server.wsgi_app, x_proto=1, x_host=1)
