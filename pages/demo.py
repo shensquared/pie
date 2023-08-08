dc2cb20a1eb42d8e208bfcb54df9dcd97aa8a570
Shen Shen <shenshen@mit.edu>
2023-08-08 16:28:03 -0400

    demo fall23 as prereq

--- pages/demo.py
+++ pages/demo.py
@@ -65,8 +65,8 @@ def show_example(course, value, marks, deptYear):
     try:
         base = "data/" + course + "/" + term
         f = base + "/classlst.xls"
-        # if term == "spring23":
-        # f = base + "/prereg.xls"
+        if term == "fall23":
+            f = base + "/prereg.xls"
         df, fig, title = data_and_chart(f, dept_or_year=deptYear)
         return (dcc.Graph(figure=fig, style={"height": "100%"}), title)
     except:
